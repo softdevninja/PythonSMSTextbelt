14fe77dcd9e006a9145d9c44c1bf57013dfc007b
Doug Zuniga <softdevninja@gmail.com>
2025-01-03 22:48:40 -0500

    Finish documentation

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
+# *_* coding: utf-8 *_*
+
+"""
+Sending SMS using Textbelt free API 
+
+References: 
+    https://textbelt.com/
+
+"""
+__version__ = "1.0.0"
+__author__ = "Doug Zuniga"
+__email__ = "softdevninja@gmail.com"
+
 import requests
 import os
 from dotenv import load_dotenv
@@ -8,6 +21,15 @@ load_dotenv()
 PHONE_NUMBER : int = os.getenv("PHONE_NUMBER")
 
 def send_message(message : str) -> None:
+    """
+    Using POST to send SMS over Textbelt free API call.
+
+    Args:
+        message (str): Message to send over API
+
+    Returns:
+        None: 
+    """
     res = requests.post('https://textbelt.com/text', {
         'phone': PHONE_NUMBER,
         'message': message,
@@ -17,6 +39,12 @@ def send_message(message : str) -> None:
     print(f'{res.json()}')
 
 def main() -> int:
+    """
+    Main entry point.  Send message via function call. 
+    
+    Returns:
+        None: N/A
+    """
     send_message("Sending SMS message ...")
     return 0
 
